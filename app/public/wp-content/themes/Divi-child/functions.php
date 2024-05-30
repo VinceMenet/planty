d53b5dfacd32f93a70a737942061c0fef2cc2af9
Vincent Menet <vincent.menet2@gmail.com>
2024-05-30 13:46:40 +0200

    version 2.0

--- app/public/wp-content/themes/Divi-child/functions.php
+++ app/public/wp-content/themes/Divi-child/functions.php
@@ -1,6 +1,6 @@
 <?php
 function mon_theme_enfant_enqueue_styles() {
-    $parent_style = 'parent-style'; // Utilisez le handle du thème parent
+    $parent_style = 'parent-style'; // Utilise le handle du thème parent
 
     wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
     wp_enqueue_style('child-style',
@@ -10,4 +10,21 @@ function mon_theme_enfant_enqueue_styles() {
     );
 }
 add_action('wp_enqueue_scripts', 'mon_theme_enfant_enqueue_styles');
+
+// Lien Admin à la connexion d'un utilisateur
+
+function add_admin_link_to_menu( $items, $args ) {
+    // Vérifie si l'utilisateur est connecté
+    if ( is_user_logged_in() ) {
+        // Crée le lien "Admin"
+        $admin_link = '<li><a href="' . admin_url() . '">' . __( 'Admin' ) . '</a></li>';
+        
+        // Ajoute le lien "Admin" aux éléments du menu
+        $items .= $admin_link;
+    }
+    
+    return $items;
+}
+add_filter( 'wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2 );
+
 ?>
